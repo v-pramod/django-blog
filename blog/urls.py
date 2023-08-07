03dddaea7b639545c2354bed3e05723cc453d41e
Pramod <vpramod1845@gmail.com>
2023-08-07 18:27:37 +0530

    added crud operations for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from blog import views
+# from .views import PostListView
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
