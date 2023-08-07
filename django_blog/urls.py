37cbf135fb0e8b41b9f7f06e254b00b359e16604
Pramod <vpramod1845@gmail.com>
2023-08-07 11:19:31 +0530

    added myprofile and logout functions

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from users.views import register
+from users.views import register, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 def register(request):
@@ -9,8 +10,13 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            return redirect('login')
     else: 
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
+
 
