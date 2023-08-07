c20e830fc2e6743e0935512ff6c486ccef962a3a
Pramod <vpramod1845@gmail.com>
2023-08-07 17:15:43 +0530

    added user profile functions

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from users.views import register, profile
 
@@ -12,3 +14,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
